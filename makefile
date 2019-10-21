59c43c2b8850c9c08eeb277d62a9d2c5fc375fae
Giovanna <glmiotto@inf.ufrgs.br>
2019-10-21 13:17:46 -0300

    Makefile, identify2, gitignore, general setup

--- makefile
+++ makefile
@@ -6,17 +6,35 @@
 #
 # NECESSARIO adaptar este esqueleto de makefile para suas necessidades.
 #
-# 
+#
 
-CC=gcc
+CC=gcc -c
 LIB_DIR=./lib
 INC_DIR=./include
 BIN_DIR=./bin
 SRC_DIR=./src
+CFLAGS=-Wall -g
+OBJS = $(LIB_DIR)/apidisk.o $(LIB_DIR)/bitmap2.o
+name = t2fs
+TARGET = $(BIN_DIR)/$(name).o
+PROG = $(SRC_DIR)/$(name).c
 
-all:
+LIB=$(LIB_DIR)/lib$(name).a
+
+all: $(TARGET)
+	ar -crs $(LIB) $^ $(OBJS)
+
+$(TARGET): $(PROG)
+		$(CC) -o $@ $< -I$(INC_DIR) $(CFLAGS)
+
+$(BIN_DIR):
+	mkdir $(BIN_DIR)
+
+tar:
+		@cd .. && tar -zcvf 287677_207758_299902.tar.gz $(name)
 
 clean:
+	mv $(OBJS) ../
 	rm -rf $(LIB_DIR)/*.a $(BIN_DIR)/*.o $(SRC_DIR)/*~ $(INC_DIR)/*~ *~
-
-
+	mv ../apidisk.o $(LIB_DIR)
+	mv ../bitmap2.o $(LIB_DIR)
--- src/t2fs.c
+++ src/t2fs.c
@@ -2,17 +2,51 @@
 /**
 */
 #include "t2fs.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* **************************************************************** */
+#define SUCCESS 0
+#define FAILED -1
+#define error() printf("Error thrown at %s:%s:%d\n",FILE,_FUNCTION__,LINE);
+
+// Debugging
+int failed(char* msg) {printf("%s\n", msg);return FAILED;}
+void print(char* msg) {printf("%s\n", msg);}
+void* null(char* msg) {printf("%s\n", msg);return (void*)NULL;}
+/* **************************************************************** */
+
+// GLOBAL VARIABLES
+FILE2 open_files[10];
+// Maximum of 10 open file handles at once
+//(***can be same file multiple times!!!)
+int fopen_count = 0;
+
+// OBS o .c original incluia nas assinaturas uma entrada DIR2
+// que assumo equivaler a um typedef int para uma handle de dir
+// mas o .h nao tem isso e nosso trabalho só usa diretorio raiz,
+// entao tirei esse argumento aqui do .c em opendir readdir e closedir
+
+
+
+/* **************************************************************** */
 
 /*-----------------------------------------------------------------------------
 Função:	Informa a identificação dos desenvolvedores do T2FS.
 -----------------------------------------------------------------------------*/
 int identify2 (char *name, int size) {
-	return -1;
+	char *nomes ="Artur Waquil Campana\t00287677\nGiovanna Lazzari Miotto\t00207758\nHenrique Chaves Pacheco\t00299902\n\0";
+	if (size < strlen(nomes)) {
+		printf("ERROR: identification requires size %zu or larger.\n", strlen(nomes));
+		return FAILED;
+	}
+	else strncpy(name, nomes, size);
+	return SUCCESS;
 }
 
 /*-----------------------------------------------------------------------------
 Função:	Formata logicamente uma partição do disco virtual t2fs_disk.dat para o sistema de
-		arquivos T2FS definido usando blocos de dados de tamanho 
+		arquivos T2FS definido usando blocos de dados de tamanho
 		corresponde a um múltiplo de setores dados por sectors_per_block.
 -----------------------------------------------------------------------------*/
 int format2(int partition, int sectors_per_block) {
@@ -36,8 +70,8 @@ int unmount(void) {
 /*-----------------------------------------------------------------------------
 Função:	Função usada para criar um novo arquivo no disco e abrí-lo,
 		sendo, nesse último aspecto, equivalente a função open2.
-		No entanto, diferentemente da open2, se filename referenciar um 
-		arquivo já existente, o mesmo terá seu conteúdo removido e 
+		No entanto, diferentemente da open2, se filename referenciar um
+		arquivo já existente, o mesmo terá seu conteúdo removido e
 		assumirá um tamanho de zero bytes.
 -----------------------------------------------------------------------------*/
 FILE2 create2 (char *filename) {
@@ -45,7 +79,7 @@ FILE2 create2 (char *filename) {
 }
 
 /*-----------------------------------------------------------------------------
-Função:	Função usada para remover (apagar) um arquivo do disco. 
+Função:	Função usada para remover (apagar) um arquivo do disco.
 -----------------------------------------------------------------------------*/
 int delete2 (char *filename) {
 	return -1;
@@ -82,7 +116,7 @@ int write2 (FILE2 handle, char *buffer, int size) {
 }
 
 /*-----------------------------------------------------------------------------
-Função:	Função usada para truncar um arquivo. Remove do arquivo 
+Função:	Função usada para truncar um arquivo. Remove do arquivo
 		todos os bytes a partir da posição atual do contador de posição
 		(current pointer), inclusive, até o seu final.
 -----------------------------------------------------------------------------*/
@@ -100,31 +134,29 @@ int seek2 (FILE2 handle, DWORD offset) {
 /*-----------------------------------------------------------------------------
 Função:	Função que abre um diretório existente no disco.
 -----------------------------------------------------------------------------*/
-DIR2 opendir2 (char *pathname) {
+int opendir2 (void) {
 	return -1;
 }
 
 /*-----------------------------------------------------------------------------
 Função:	Função usada para ler as entradas de um diretório.
 -----------------------------------------------------------------------------*/
-int readdir2 (DIR2 handle, DIRENT2 *dentry) {
+int readdir2 (DIRENT2 *dentry) {
 	return -1;
 }
 
 /*-----------------------------------------------------------------------------
 Função:	Função usada para fechar um diretório.
 -----------------------------------------------------------------------------*/
-int closedir2 (DIR2 handle) {
+int closedir2 (void) {
 	return -1;
 }
 
 /*-----------------------------------------------------------------------------
 Função:	Função usada para criar um caminho alternativo (softlink) com
-		o nome dado por linkname (relativo ou absoluto) para um 
+		o nome dado por linkname (relativo ou absoluto) para um
 		arquivo ou diretório fornecido por filename.
 -----------------------------------------------------------------------------*/
 int ln2 (char *linkname, char *filename) {
 	return -1;
 }
-
-
